d961670bc3b6e740b8a6d0f14b6bf34aca492a9c
Otto Rohenkohl <message@rohenkohl.dev>
2025-02-26 11:20:33 +0100

    feat: adding logging

--- src/main/kotlin/dev/rohenkohl/domain/service/LudolphService.kt
+++ src/main/kotlin/dev/rohenkohl/domain/service/LudolphService.kt
@@ -1,5 +1,6 @@
 package dev.rohenkohl.domain.service
 
+import io.quarkus.logging.Log
 import jakarta.enterprise.context.ApplicationScoped
 import org.eclipse.microprofile.config.ConfigProvider
 
@@ -18,13 +19,25 @@ class LudolphService(val discordService: DiscordService, val investspielService:
         val bought = newAssets.minus(storedAssets)
         val sold = storedAssets.minus(newAssets)
 
-        if (bought.isNotEmpty() || sold.isNotEmpty()) discordService.announceChange(bought, sold)
+        if (bought.isNotEmpty() || sold.isNotEmpty()) {
+            Log.info("Ludolph has made changes; bought $bought, sold $sold")
+
+            discordService.announceChange(bought, sold)
+        } else {
+            Log.debug("No Changes in the portfolio")
+        }
 
         this.storedAssets = newAssets
     }
 
     fun updatePosition() {
-        if (newPosition != storedPosition) discordService.announcePosition(storedPosition, newPosition)
+        if (newPosition != storedPosition) {
+            Log.info("Ludolph has switched rank; before $storedPosition, after $newPosition")
+
+            discordService.announcePosition(storedPosition, newPosition)
+        } else {
+            Log.debug("No Changes in the position")
+        }
 
         this.storedPosition = newPosition
     }
